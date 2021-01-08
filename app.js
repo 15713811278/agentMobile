4f4f75c0874d6f45294c2b8e1480e473fd05acdc
zhangzonglu <zhangzonglu@powerpms.com>
2021-01-08 20:12:15 +0800

    init2

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-// app.js
+//app.js
 App({
-  onLaunch() {
+  onLaunch: function () {
     // 展示本地存储能力
-    const logs = wx.getStorageSync('logs') || []
+    var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
 
---
+++ pages/personal/personal.js
@@ -0,0 +1,77 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    menu:["首页","电视","购物","我的"],
+    detail:["我是首页","我是电视","我是购物","我是我的"],
+    activeIndex:0
+  },
+  change(e){
+    console.log(e)
+    this.setData({
+      activeIndex:e.target.id
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  }
+})
