b03e9e46534610bd5feafe5ea1612616501da5c1
zhangzonglu <zhangzonglu@powerpms.com>
2021-01-12 17:27:56 +0800

    1.12授权登录

--- pages/personal/personal.js
+++ pages/personal/personal.js
@@ -1,6 +1,6 @@
 //index.js
 //获取应用实例
-// const app = getApp()
+const app = getApp()
 
 Page({
 
@@ -8,7 +8,8 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    onOff: true, //true代表未登录  false代表登录
+    userInfo:{}
   },
   toUser() {
     wx.navigateTo({
@@ -25,17 +26,42 @@ Page({
       url: '../../pages/account/account',
     })
   },
-  toMoncome(){
+  toMoncome() {
     wx.navigateTo({
       url: '../../pages/moncome/moncome',
     })
   },
-
+  getData() {
+    wx.request({
+      url: 'https://pay.powerpms.com/api/agent/wx/user/briefInfo',
+      method: "get",
+      success: (res) => {
+        console.log(res)
+      }
+    })
+  },
+  //登录授权
+  login(e) {
+    console.log(e)
+    if (e.detail.errMsg == "getUserInfo:fail auth deny") {
+      //拒绝授权  
+      wx.showToast({
+        title: '未登录',
+        icon: "none"
+      })
+      return false
+    }
+    //同意授权
+    this.setData({
+      userInfo:e.detail.userInfo,
+      onOff:false
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.getData();
   },
 
   /**
@@ -49,41 +75,29 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    let _this = this;
+    wx.getSetting({
+      success(res) {
+        if (res.authSetting['scope.userInfo']) {
+          //证明已经授权
+          wx.getUserInfo({
+            complete: (res) => {
+              console.log(res, '授权了获取用户信息')
+              _this.setData({
+                onOff: false,
+                userInfo:res.userInfo
+              })
+            }
+          })
+        } else {
+          //没有授权
+          wx.showToast({
+            title: '未登录',
+            icon: "none"
+          })
+        }
+      }
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
